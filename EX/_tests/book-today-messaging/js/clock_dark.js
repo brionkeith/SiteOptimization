98cda2191bd421cf35f0a6e0ef7f878b6d9c6ca1
brionkeith <brion.mills@ihg.com>
2015-01-26 14:27:16 -0500

    Create clock_dark.js

---
+++ EX/_tests/book-today-messaging/js/clock_dark.js
@@ -0,0 +1,10 @@
+(function() {
+  jQuery(document).ready(function(jQuery) {
+    var msgWrap;
+    msgWrap = jQuery("<div class=\"msgWrapper\"><span class=\"msgWrapper-btmMsg\">MAKE YOUR RESERVATION TODAY AND LOCK IN THIS LOW PRICE<span class=\"msgWrapper-ltdAvail\">Room Availability is Limited</span></span></div>");
+    msgWrap.insertBefore("#roomRateRoomType");
+    msgWrap.delay(600).fadeIn(700);
+    jQuery(".msgWrapper:eq(1)").remove();
+  });
+
+}).call(this);
