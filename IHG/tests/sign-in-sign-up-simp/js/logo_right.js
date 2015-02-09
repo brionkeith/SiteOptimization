ce2d36274fbd6e910b1d672343f44a8cae8a4c93
Brion MIlls <brion.mills@ihg.com>
2015-02-09 16:06:56 -0500

    Updates: Logo Right Dependencies

--- IHG/tests/sign-in-sign-up-simp/js/logo_right.js
+++ IHG/tests/sign-in-sign-up-simp/js/logo_right.js
@@ -1,7 +1,7 @@
 (function() {
   jQuery(document).ready(function(jQuery) {
 
-    jQuery('#identityBlock:eq(0) .heavyText span').html('We\'ll Fill Out the Form. You\'ll Get the Points. Sign In Now.'); // Change default HTML text
+    jQuery('#identityBlock:eq(0) .heavyText span').html('Sign in IHG<sup>®</sup> Rewards Club Members'); // Change default HTML text
     jQuery('#identityBlock:eq(0) .heavyText, .signIn').wrapAll('<div class=\"rememberWrap\" />'); // Wrap both div tags into a single div
     //jQuery('#identityBlock:eq(0) .rememberWrap .heavyText').eq(1).remove(); // Remove first dulicate
     //jQuery('#identityBlock:eq(0) .rememberWrap .heavyText:eq(1)').remove(); // Remove duplicate
@@ -11,12 +11,12 @@
     jQuery('.signIn label, .signIn input#remember').wrapAll('<div class=\"rememberCheck\"></div>'); // Wrap label and input into a single div
     jQuery('.signIn div:eq(3)').addClass('forgotRememberPin'); // Add class name to div for specific targeting
     
-    jQuery('#frmGuestInfo #identityBlock .heavyText span:eq(0)').html('Get More from Your Stay. Join IHG<sup>®</sup> Rewards Club.'); // Change default HTML text
+    jQuery('#frmGuestInfo #identityBlock .heavyText span:eq(0)').html('Join IHG<sup>®</sup> Rewards Club. Earn Points &amp; Get Rewarded.'); // Change default HTML text
     jQuery('#frmGuestInfo #identityBlock #anonId .heavyText').prependTo('.joinNow'); // Move header text into .joinNow for more accurate positioning
     
     var POINTS = jQuery('#frmGuestInfo #identityBlock .heavyText:eq(1) span').text(); // Get points for earn messaging
     jQuery('#frmGuestInfo #identityBlock .heavyText:eq(1)').html('This reservation could earn you ' + '<strong>' + POINTS + '</strong>' + ' points towards a free night.'); // Change default text and add POINTS var
-    jQuery('#frmGuestInfo #identityBlock #anonId .joinnowtext label').html('Sign me up now. Look for your new member info on the confirmation page. <br>Read the<a href=\"#\" class=\"pcrTerms\"> IHG<sup>®</sup> Rewards Club Terms &amp; Conditions.</a>'); // Change default HTML text
+    jQuery('#frmGuestInfo #identityBlock #anonId .joinnowtext label').html('<strong>Sign me up now.</strong> Look for your new member info on the confirmation page. <br>Read the<a href=\"#\" class=\"pcrTerms\"> IHG<sup>®</sup> Rewards Club Terms &amp; Conditions.</a>'); // Change default HTML text
 
     // Show Forgot Pin dialog
     jQuery('#UHF_createPinLink').click(function(event) {
