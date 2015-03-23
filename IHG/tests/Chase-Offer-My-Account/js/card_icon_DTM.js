a1ae5de6e01b9be5211616a1b0f18c87531a6f0c
Brion MIlls <brion.mills@ihg.com>
2015-03-23 13:55:23 -0400

    Edits: parseInt() partially working

--- IHG/tests/Chase-Offer-My-Account/js/card_icon_DTM.js
+++ IHG/tests/Chase-Offer-My-Account/js/card_icon_DTM.js
@@ -3,15 +3,18 @@ jQuery(document).ready(function(jQuery) {
 	var pcINTERACT  = jQuery('#myMessages.rewardsClubHome #pcrInteract'); // Interact marketing module
 	var chaseOffer  = jQuery('<div class="chaseOffer"></div>');
 	var iconCC      = jQuery('<img src="../img/icon_credit_card.png">');
-
 	var chaseMsg    = jQuery('<p>Earn 70,000 bonus points. Plus a Free Night every year.</p>');
 	var learnMore   = jQuery('<a href=\"https://creditcards.chase.com/a1/ihg/ihg70/?CELL=6FDG&cm_sp=OSMAM-6C-US-EN-MYM-X-MHR-ChaseMasterCard-70offer-S\"> Learn More. </a>');
 	var currentPtsTxt  = jQuery('<p class="rcPoints">Current Points:</p>');
 
-	var chasePoints = jQuery(70000);
+	var bonusPoints = 70000;
 	var ptsBalance  = jQuery('.pointsBalanceLabel .value.large.withCommas').clone(); // Clone current RC points balance
+	var bonusPtsTxt = jQuery('<span class="bonusPtsTxt">+ Bonus Points:<span class=\"bonusPts\">' + bonusPoints + '</span></span>');
 
-	var bonusPtsTxt = jQuery('<span class="bonusPtsTxt">+ Bonus Points:<span class=\"chasePts\">' + chasePoints + '</span></span>'); // NOT WORKING CORRECTLY
+	var strParse    = parseInt(ptsBalance, 10); // NOT WORKING: Output should be current RC balance
+	alert(strParse);
+
+	var totalPts    = jQuery(strParse + bonusPoints); // NOT WORKING
 
 	jQuery(pcINTERACT).remove(); //remove Interact container
 	jQuery(chaseOffer).insertAfter('#myMessages.rewardsClubHome h3');
@@ -20,7 +23,18 @@ jQuery(document).ready(function(jQuery) {
 	jQuery(learnMore).appendTo(chaseMsg);
 
 	jQuery(currentPtsTxt).insertAfter(chaseMsg);
-	jQuery(ptsBalance).insertAfter(currentPtsTxt);   // place RC points balance after 'Current Points' text
-	jQuery(bonusPtsTxt).appendTo(ptsBalance);  // place Bonus Points text after RC balance
+	jQuery(ptsBalance).insertAfter(currentPtsTxt); // place RC points balance after 'Current Points' text
+	jQuery(bonusPtsTxt).appendTo(ptsBalance);      // place Bonus Points text after RC balance
+
+	jQuery(totalPts).insertAfter(bonusPtsTxt); // NOT WORKING
+
+    // regex: add comma to separate value as needed
+    jQuery.fn.digits = function () {
+        return this.each(function () {
+            jQuery(this).text(jQuery(this).text().replace(/(\d)(?=(\d\d\d)+(?!\d))/g, "$1,"));
+        });
+    };
+
+    jQuery('.bonusPts').digits(); // call 'digits' function and apply to 'chasePts'
 
 });
---
+++ IHG/tests/Chase-Offer-My-Account/js/card_icon_DTM_v2.js
@@ -0,0 +1,36 @@
+jQuery(document).ready(function(jQuery) {
+	
+	var pcINTERACT  	= jQuery('#myMessages.rewardsClubHome #pcrInteract'); // Interact marketing module
+	var chaseOffer  	= jQuery('<div class="chaseOffer"></div>');
+	var iconCC 			= jQuery('<img src="../img/icon_credit_card.png">');
+	var chaseMsg		= jQuery('<p class="chaseMsg">Earn 70,000 bonus points. Plus a Free Night every year.</p>');
+	var learnMore		= jQuery('<a href="https://creditcards.chase.com/a1/ihg/ihg70/?CELL=6FDG&cm_sp=OSMAM-6C-US-EN-MYM-X-MHR-ChaseMasterCard-70offer-S"> Learn More. </a>');
+	var currentPtsTxt	= jQuery('<p class="currentPts">Current Points:</p>');
+	var ptsBalance 		= jQuery('.pointsBalanceLabel .value.large.withCommas').clone().text(); // Clone current RC points balance
+	var strParse    	= parseInt(ptsBalance, 10); // * Output should be current RC balance: Not rounded number
+	// alert(strParse);
+	var bonusPoints		= 70000;
+	var bonusPtsTxt 	= jQuery('<span class="bonusPtsTxt">+ Bonus Points:<span class=\"bonusPts\">' + bonusPoints + ' = ' + '</span>' + '</span><span class="string">Points</span>');	
+	var totalPts    	= jQuery(strParse + bonusPoints); // sum total of Bonus points (70,000) and current RC points
+
+	jQuery(pcINTERACT).remove(); //remove Interact container
+	jQuery(chaseOffer).insertAfter('#myMessages.rewardsClubHome h3');
+	jQuery(iconCC).appendTo(chaseOffer);
+	jQuery(chaseMsg).appendTo(chaseOffer);
+	jQuery(learnMore).appendTo(chaseMsg);
+
+	jQuery(currentPtsTxt).insertAfter(chaseMsg);	// insert "Current Points" text after Chase message
+	jQuery(strParse).appendTo(currentPtsTxt);		// insert RC points balance after 'Current Points' text
+	jQuery(bonusPtsTxt).insertAfter(currentPtsTxt);	// insert Bonus Points text after RC balance
+	jQuery(totalPts).appendTo(bonusPtsTxt);		    // insert Bonus points and sum of total points
+
+    // regex: add comma to separate value as needed
+    jQuery.fn.digits = function () {
+        return this.each(function () {
+            jQuery(this).text(jQuery(this).text().replace(/(\d)(?=(\d\d\d)+(?!\d))/g, "$1,"));
+        });
+    };
+
+    jQuery('.bonusPts').digits(); // call 'digits' function and apply to 'bonusPts'
+
+});
