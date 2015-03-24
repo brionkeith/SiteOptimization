7ea93695967ba3a3b6b8a1bac153b162cc290825
Brion MIlls <brion.mills@ihg.com>
2015-03-24 15:13:25 -0400

    Code Updates
    1. Added Mbox click tracking
    2. Modified experiences B/C to validate loginType "implicit" to handle
    styling for messaging and button position.

--- IHG/tests/RC-Join-CTA-Emphasis/js/linear.js
+++ IHG/tests/RC-Join-CTA-Emphasis/js/linear.js
@@ -3,4 +3,22 @@ jQuery(document).ready(function(jQuery) {
 
 	jQuery(RCjoinMod).detach();
 	jQuery(RCjoinMod).appendTo('.contentWrapperHeader');
+
+    // validate loginType: implicit
+    if (trackingJson.loginType == 'implicit') {
+
+        jQuery('.marketingModuleState').css({
+            'text-align': 'right' // align parent container
+        });
+
+        jQuery('.marketingModuleState h3:contains("Already a Member?")').css({
+            'margin-right': '13px', // increases marging between button
+            'text-align': 'right', // override default text alignment
+            'width': '80%' // prevent text from collapsing
+        });
+
+        jQuery('.marketingModuleState #signInSubmit').css({
+            'margin-top': '0' // override default top margin
+        });
+    }		
 });
