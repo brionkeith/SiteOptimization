27e4f133346031ed94d2ac0a1f15c1212050ccd2
Brion MIlls <brion.mills@ihg.com>
2016-03-24 10:41:54 -0400

    IHG: App Download Photo
    Final files (CSS, SCSS, JS).

---
+++ IHG/tests/App-Download-Headline-Text/js/experience_B.js
@@ -0,0 +1,7 @@
+jQuery(document).ready(function($) {
+	var taoDownloadIntro = jQuery ('.downloadintro');
+	// replace download text
+	jQuery(taoDownloadIntro).html('<b>Find, Book and Manage</b> your stay<br> with the <b>Free IHG<sup>&reg;</sup> App</b>.').css({
+		'display': 'block'
+	});
+});
---
+++ IHG/tests/App-Download-Headline-Text/js/experience_C.js
@@ -0,0 +1,7 @@
+jQuery(document).ready(function($) {
+	var taoDownloadIntro = jQuery ('.downloadintro');
+	// replace download text
+	jQuery(taoDownloadIntro).html('<b>Manage Your Stay</b> Anytime, Anywhere<br> with the <b>Free IHG<sup>&reg;</sup> App</b>.').css({
+		'display': 'block'
+	});
+});
