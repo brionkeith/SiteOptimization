8a1639e0e89178895a21a82e9c2cd10c45bf7906
Brion MIlls <brion.mills@ihg.com>
2016-03-24 10:56:19 -0400

    IHG: App Download Photo
    Final code: CSS, JS, SCSS

---
+++ IHG/tests/App-Download-Photo/js/experience_C.js
@@ -0,0 +1,36 @@
+jQuery(document).ready(function ($) {
+    // Modify headline text
+    jQuery('.appdl-bkgd .ihgtranslatortext:eq(0)').prepend('<p class="taoDownloadintro">Join the <strong>Millions of People</strong> Who Have<br> Downloaded the Free IHG&reg; App.</p>');
+
+    // Insert new "standard rates" text
+    jQuery('<p class="taoDataRates">Standard message and data rates may apply. We will not use your information for marketing purposes.</p>').insertAfter('.sms_modal_enter_number .appdl-txt');
+
+    // Reverse position of form elements
+    jQuery('.cellphonenumber').insertBefore('.countrydrop');
+
+    // Remove first (default) option
+    jQuery('#sms_modal_form_idd option:eq(0)').remove();
+
+    // Remove this to facilitate floating the 'send link' button
+    jQuery('.countrydrop').next('.clearingDiv').remove();
+
+    // Update 'number' label text
+    jQuery('.requiredField_indicator:eq(0)').html('<strong>Mobile Number</strong> (No Dashes)');
+
+    // Update 'country' label text
+    jQuery('.requiredField_indicator:eq(1)').html('<strong>Mobile Country</strong>');
+
+    // Remove DIV to accomodate floating the star rating
+    jQuery('.appdl-rating-txt').remove();
+
+    // Replace star rating image
+    jQuery('.appdl-stars img').replaceWith('<img src="http://prodcache.internal.ihg.com/content/dam/mobile/6c/en/us/app/icon_stars.png">');
+
+    // Add new text
+    jQuery('<p>4+ Average App Store Rating</p>').appendTo('.appdl-stars');
+
+    jQuery( document ).ajaxComplete(function( event,request, settings ) {
+      // Remove messaging after AJAX COMPLETE
+      jQuery('.downloadintro').remove();
+    });
+});
