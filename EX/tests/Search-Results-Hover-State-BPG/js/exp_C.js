95a996d073febf5e5e7289b30288491197a97c9c
Brion MIlls <brion.mills@ihg.com>
2016-03-24 10:33:58 -0400

    EX: Search Results Hover State BPG
    Final Code

---
+++ EX/tests/Search-Results-Hover-State-BPG/js/exp_C.js
@@ -0,0 +1,28 @@
+jQuery(document).ready(function (jQuery) {
+
+	var taoCheckRates = jQuery('.row.checkRates');
+	jQuery('<div class="taoCheckRatesBPG"><span>Best Price Guaranteed.</span></div>').appendTo(taoCheckRates);
+
+	/*jQuery('.checkRates .bookAnchor').on('mouseover', function(){
+		jQuery(this).closest('.row').find('.taoCheckRatesBPG').css('display', 'block');
+	});
+
+	jQuery('.checkRates .bookAnchor').on('mouseout', function(){
+		jQuery(this).closest('.row').find('.taoCheckRatesBPG').css('display', 'none');
+	});*/
+
+  // Run again after AJAX call
+  jQuery(document).ajaxComplete(function (event, request, settings) {
+    var taoCheckRates = jQuery('.row.checkRates');
+    jQuery('<div class="taoCheckRatesBPG"><span>Best Price Guaranteed.</span></div>').appendTo(taoCheckRates);
+
+    /*jQuery('.checkRates .bookAnchor').on('mouseover', function(){
+      jQuery(this).closest('.row').find('.taoCheckRatesBPG').css('display', 'block');
+    });
+
+    jQuery('.checkRates .bookAnchor').on('mouseout', function(){
+      jQuery(this).closest('.row').find('.taoCheckRatesBPG').css('display', 'none');
+    });*/
+  });  
+
+});
