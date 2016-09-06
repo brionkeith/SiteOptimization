d181170e5ac98af8f090ce95991ea27878ff4d18
Brion MIlls <brion.mills@ihg.com>
2016-09-06 10:38:09 -0400

    New commit

--- IHG/tests/Triptease-Module/js/exp_C.js
+++ IHG/tests/Triptease-Module/js/exp_C.js
@@ -1,47 +1,441 @@
-function mboxPixelTrack(mbox) {
-    var d = new Date();
-    var ub = mboxFactoryDefault.getUrlBuilder().clone();
-    ub.addParameter("mbox", mbox);
-    ub.addParameter('mboxTime', d.getTime() - (d.getTimezoneOffset() * 60000));
-    ub.addParameters(Array.prototype.slice.call(arguments).slice(1));
-    var img = new Image(); img.src = ub.buildUrl().replace("/mbox/undefined", "/mbox/ajax");
-    img.style.display = "none"; if (document.body) document.body.insertBefore(img,document.body.firstChild); }
+function taoTripTeaseModule() {
 
-whenReady = function(readyFunc,actionFunc,options) {
-    if (readyFunc()) { actionFunc.apply(this);
-    } else {
-        if (!options) options = {};
-        if (!options.current) options.current = 0;
-        if (!options.max) options.max = 6000;
-        if (!options.interval) options.interval = 1000;
-        if (options.current < options.max) { setTimeout(function() { options.current += options.interval;
-            whenReady(readyFunc,actionFunc,options); },options.interval);
-        } else if (options.ontimeout) {
-            options.ontimeout() } }; return true;
-};
+    // Replace original TT container with this
+    jQuery('.row.trip-tease-container').each(function () {
 
-whenReady(
-    function() { return window.jQuery && !!jQuery('.price-fighter-widget')[0]; },
-    function() {
-        // get the widget placeholder
-        var widget = jQuery('.price-fighter-widget');
-        // hold on to the widget's parent element
-        var newLocation = jQuery('#reserveNowMsg');
-        // clone an empty placeholder
-        var newWidget = widget.clone().empty();
-        // remove the old widget
-        widget.remove();
-        // configure and append the new widget
-        newWidget.attr('data-pf-layout', 'ihg-wide');
-        newLocation.after(newWidget);
-        newWidget.show();
-        // reload
-        jQuery.getScript('https://stg-widget.triptease.net/loader.js?token=ecf0ef4b7731c56dfebdfaef246c94bbd34afab2');
-    }, // action function
-    {   //this is only necessary if you need to do something on timeout.
-        ontimeout: function() {
-            console.log('*** Timing out ***');
-            //jQuery('.price-fighter-widget').css('visibility','visible');
+        // PL: find the closest .resRow and input[type=hidden].selectedHotelCode => the value is what we need to put in data-pf-property
+        var result = jQuery(this).closest('.resRow');
+        var propertyCode = result.find('input[type=hidden].selectedHotelCode').val();
+
+        // See if the propertyCode is in the list of Hotel Codes. If not, then
+        // return true and skip to the next .row.trip-tease-container.
+        if (jQuery.inArray( propertyCode, taoAllHotelCodes ) < 0 ) {
+            return true;
         }
-    }   //ontimeout
-); // whenReady
+
+        // PL: use the property code in the show button (so that we know which widget to activate
+        result.find('.taoCompare').data('pf-property', propertyCode.toLowerCase());
+
+        // PL: use a global roomkey_config variable (found when inspecting the sources) to extract the params we need
+        var checkin = roomkey_config.check_in_val;
+        var checkout = roomkey_config.check_out_val;
+        var adults = roomkey_config.guests_val;
+        var children = 0; //TODO - is this really correct
+
+        // PL: get the direct price
+        //var direct = result.find('.txtFromNow .mainCurrencyUnitValue.cc_number').text().trim();
+        var direct = result.find('.priceNow .mainCurrencyUnitValue.cc_number').text().trim();
+        console.log('Hotel: ', propertyCode, ' checkin: ', checkin, ' checkout: ', checkout, ' adults: ', adults );
+        jQuery(this).replaceWith(
+            '<div '+
+            'class="price-fighter-widget ' + propertyCode.toLowerCase() + '"' +
+            'data-pf-hotelkey="6e7dd50235b6f9def5cbf723d630411f0507c358"' +
+            'data-pf-property="' + propertyCode +'"' +
+            'data-pf-currency="GBP"' +
+            'data-pf-checkin="' + checkin + '"' +
+            'data-pf-checkout="' + checkout+'"' +
+            'data-pf-direct-price="' + direct + '"'+
+            'data-pf-room-rate=""'+
+            'data-pf-adults="' + adults + '"' +
+            'data-pf-rooms="1"' +
+            'data-pf-children="' + children + '"' +
+            'data-pf-layout="skeleton-1"' +            'data-pf-custom-css="https://prodcache.internal.ihg.com/content/dam/etc/media_library/cn/0/cn/css/sr/tao-triptease-exp-C.css">' +
+            '</div>');
+    });
+
+    // Move 'price-fighter-widget' into each '.pillBox' container
+    jQuery('.pillBox').each(function(){
+
+        jQuery(this).find('.price-fighter-widget').appendTo(jQuery(this) );
+
+    });
+    // Remove widget when no dates are available or have not been selected
+    jQuery('.priceInfoArea .bulkAvailLinkBox').parent().next().next().next().remove('.price-fighter-widget');
+    jQuery('.priceInfoArea .priceMsgNotAvail').parent().parent().next().next().next().remove('.price-fighter-widget');
+
+    //PL reload paperboy
+    jQuery.getScript('https://paperboy.triptease.net/IHG.js');
+
+}
+
+jQuery(document).ready(function (jQuery){
+
+    taoTripTeaseModule();
+
+    jQuery('body').on('click','#showMoreLink, #showAllLink', function(){
+
+        // Create a boolean that we keep track of just this click event and
+        // make sure this code only runs when the user clicks on one of these
+        // two buttons, but doesn't interfere with the other Ajax calls.
+        var taoShowClick = true;
+
+        // Execute code if show more or show all buttons are clicked
+        jQuery(document).ajaxComplete(function (event, request, settings) {
+
+            if (taoShowClick) {
+                taoTripTeaseModule();
+                // Paperboy.PriceCheck.deactivate(document.querySelectorAll('.price-fighter-widget'));
+                // Paperboy.PriceCheck.activate(document.querySelectorAll('.price-fighter-widget'));
+                taoShowClick = false;
+            }
+
+        }); // End ajaxComplete();
+
+    });
+
+});
+
+// create an array that contains all of the hotel codes that are part of this
+// campaign so we can make sure this happens only on the hotels it should.
+var taoAllHotelCodes = [
+    "ABZAA",
+    "ABZCC",
+    "ABZEC",
+    "ABZEX",
+    "ABZIG",
+    "ABZWH",
+    "AMSAA",
+    "AMSAM",
+    "AMSAP",
+    "AMSAS",
+    "AMSEA",
+    "AMSNL",
+    "AMSNT",
+    "AMSOP",
+    "AMSPC",
+    "AMSUT",
+    "ANRBE",
+    "ANRHE",
+    "ASDKE",
+    "ASDUK",
+    "AYLBK",
+    "BBSHP",
+    "BFSAN",
+    "BFSAS",
+    "BFSEX",
+    "BFYVL",
+    "BHTSF",
+    "BHXAP",
+    "BHXBP",
+    "BHXBR",
+    "BHXBS",
+    "BHXCS",
+    "BHXCT",
+    "BHXEC",
+    "BHXGB",
+    "BHXIN",
+    "BHXNC",
+    "BHXPW",
+    "BHXRD",
+    "BHXSB",
+    "BHXSC",
+    "BHXSH",
+    "BHXSL",
+    "BHXSW",
+    "BHXTS",
+    "BHXWA",
+    "BHXWM",
+    "BHXWS",
+    "BRDWD",
+    "BREGC",
+    "BRSAP",
+    "BRSCC",
+    "BRSCT",
+    "BRSFR",
+    "BRSNR",
+    "BRUAP",
+    "BRUBE",
+    "BRUBR",
+    "BRUZM",
+    "BTNBL",
+    "BTNCC",
+    "BUTUK",
+    "CAXDM",
+    "CBGCC",
+    "CBGDK",
+    "CBGIM",
+    "CBMUK",
+    "CDFAP",
+    "CDFBA",
+    "CDFCY",
+    "CHETR",
+    "CHETS",
+    "CHEUK",
+    "CHEWR",
+    "CRYUK",
+    "CTBUK",
+    "CVTAP",
+    "CVTHR",
+    "CVTKW",
+    "CWLCN",
+    "DBLBL",
+    "DBLDD",
+    "DBLEX",
+    "DBLNP",
+    "DBYRL",
+    "DBYUK",
+    "DFBKE",
+    "DFBUK",
+    "DNDEE",
+    "DNFUK",
+    "DNHPR",
+    "DOTWC",
+    "DSAUK",
+    "DSAWW",
+    "EAMLC",
+    "EAMLN",
+    "EDBCR",
+    "EDBLH",
+    "EDBPP",
+    "EDBRM",
+    "EDBUK",
+    "EDIAP",
+    "EDINI",
+    "EDITE",
+    "EINNL",
+    "EKBUK",
+    "ELLUK",
+    "EMAMW",
+    "EMART",
+    "EMASS",
+    "EMAUK",
+    "EMLAP",
+    "EXEUK",
+    "FABHT",
+    "FHLBK",
+    "GHNCL",
+    "GLCCH",
+    "GLCEA",
+    "GLCUK",
+    "GLNUK",
+    "GLWAP",
+    "GLWCR",
+    "GLWGC",
+    "GLWGK",
+    "GLWIN",
+    "GLWRE",
+    "GLWST",
+    "GLWTH",
+    "GNTBE",
+    "GNTEP",
+    "GSYEX",
+    "GUISU",
+    "HAGMS",
+    "HRTUK",
+    "HSLBE",
+    "HSLTL",
+    "HUYNH",
+    "HUYUK",
+    "HYCBK",
+    "HYCCP",
+    "IJMKE",
+    "INVUK",
+    "IPWEA",
+    "IPWWE",
+    "KENMI",
+    "LCTUK",
+    "LCTWS",
+    "LDNNL",
+    "LEEBH",
+    "LEECC",
+    "LEECD",
+    "LEEEA",
+    "LEEGF",
+    "LEETL",
+    "LEEUK",
+    "LEIEX",
+    "LGEMS",
+    "LGWAP",
+    "LGWUK",
+    "LGWWO",
+    "LHFUK",
+    "LONAH",
+    "LONBF",
+    "LONBL",
+    "LONBW",
+    "LONCB",
+    "LONCF",
+    "LONCL",
+    "LONCR",
+    "LONCT",
+    "LONCY",
+    "LONEA",
+    "LONEC",
+    "LONEL",
+    "LONEP",
+    "LONET",
+    "LONFR",
+    "LONGG",
+    "LONGH",
+    "LONGM",
+    "LONHA",
+    "LONHI",
+    "LONHS",
+    "LONHT",
+    "LONHU",
+    "LONIN",
+    "LONJU",
+    "LONKC",
+    "LONKE",
+    "LONKT",
+    "LONLH",
+    "LONLK",
+    "LONLT",
+    "LONLU",
+    "LONMF",
+    "LONMR",
+    "LONNO",
+    "LONPM",
+    "LONPW",
+    "LONRP",
+    "LONSA",
+    "LONSF",
+    "LONSG",
+    "LONSH",
+    "LONSI",
+    "LONST",
+    "LONSU",
+    "LONSW",
+    "LONTF",
+    "LONUK",
+    "LONVA",
+    "LONVE",
+    "LONVH",
+    "LONWA",
+    "LONWD",
+    "LONWE",
+    "LONWI",
+    "LONWS",
+    "LONWX",
+    "LONXC",
+    "LONXL",
+    "LPLAD",
+    "LPLAP",
+    "LPLCP",
+    "LPLHL",
+    "LPLIL",
+    "LPLKW",
+    "LPLLS",
+    "LPLRL",
+    "LPLUK",
+    "LTNAP",
+    "LTNDU",
+    "LTNHH",
+    "LTNSO",
+    "LTNSP",
+    "LYXFX",
+    "MAIKE",
+    "MANCC",
+    "MANUK",
+    "MCHAS",
+    "MCHCP",
+    "MCHEA",
+    "MCHMA",
+    "MCHMC",
+    "MCHOR",
+    "MCHSA",
+    "MCHSK",
+    "MCHSS",
+    "MCHWT",
+    "MCSUK",
+    "MDNUK",
+    "MECVM",
+    "MLKBK",
+    "MLKEP",
+    "MLKLR",
+    "MSTNL",
+    "NCLIN",
+    "NCLJB",
+    "NCLJM",
+    "NCLMS",
+    "NCLSQ",
+    "NCLUK",
+    "NCLWS",
+    "NOTDE",
+    "NOTUK",
+    "NOTWS",
+    "NPTCS",
+    "NPTUK",
+    "NWIAP",
+    "NWICR",
+    "NWINF",
+    "NWISV",
+    "OFDCN",
+    "OFDKS",
+    "OFDUK",
+    "ORKKI",
+    "ORMGP",
+    "ORMKP",
+    "ORMMJ",
+    "ORMUK",
+    "OSTBE",
+    "PLHUK",
+    "PMEGQ",
+    "PMEHP",
+    "PMESP",
+    "POOUK",
+    "PSTLC",
+    "PTLDU",
+    "PTRUK",
+    "PTRWE",
+    "QQZBA",
+    "RCSKE",
+    "REASO",
+    "REATR",
+    "REAUK",
+    "REAWS",
+    "RTMCS",
+    "RUGNH",
+    "SENCP",
+    "SENES",
+    "SENHI",
+    "SFDUA",
+    "SFFSO",
+    "SIRUK",
+    "SITBR",
+    "SOABR",
+    "SOAEL",
+    "SOAFA",
+    "SOAHP",
+    "SOAUK",
+    "SOAWC",
+    "STDEX",
+    "STDHM",
+    "STNCO",
+    "STNDC",
+    "SWICC",
+    "SWIMR",
+    "SWIWE",
+    "SWSSA",
+    "SZDBR",
+    "TELUK",
+    "TNTSS",
+    "TNTUK",
+    "WARLS",
+    "WARUK",
+    "WDPUK",
+    "WRTHA",
+    "WRTRN",
+    "WRTUK",
+    "WXFBT",
+    "XHUHR",
+    "XNVUK",
+    "XPTPS",
+    "XQDBF",
+    "XQLCR",
+    "XSRSP",
+    "XVGDL",
+    "XVGNO",
+    "XVJMH",
+    "XVJUK",
+    "XWDEX",
+    "XWDWY",
+    "XWHSF",
+    "XWHUK",
+    "YORTR",
+    "YORUK",
+    "YORYK",
+    "ZYMEX",
+    "EDIIN",
+    "LTNST"
+];
